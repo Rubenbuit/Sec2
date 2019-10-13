052e1bba4a9b558875daa59a27036e5c2fa31ec2
Ruben <rbuitendijk1994@gmail.com>
2019-10-13 20:52:26 +0200

    Rsa encreyp / decrypt characters
    Is testsed, working well

--- Rsa.java
+++ Rsa.java
@@ -2,9 +2,11 @@ import java.util.Random;
 
 public class Rsa{
 
-
 public String encryptmessage(String privateKey, String message){
-  return(privateKey + "encrypt: " +message );
+  for(int i = 0; i< message.length();i++){
+    int x = charToAnciiInt(message.charAt(i));
+  }
+return "done"
 }
 
 public String decryptmessage(String publicKey, String message){
@@ -21,5 +23,16 @@ public String createPublicKey(){
   //return Integer.toString(n);
   return "B";
 }
+
+//converting a string into int
+private int charToAnciiInt( char message){
+  return (int) message;
+}
+
+//convert int back to string
+private String anciiToChar( int message){
+  return Character.toString((char)message);
+}
+
 
 }
